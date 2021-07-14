3b0bd2cfb1948ec6fc216216a83a10f040f191e4
herbatataszatana <anma.swiatkowska@gmail.com>
2021-07-14 20:48:21 +0100

    commit

---
+++ nextInLineFunction.js
@@ -0,0 +1,8 @@
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    var removed = arr.shift();
+    return item;
+    // Only change code above this line
+  }
+  
