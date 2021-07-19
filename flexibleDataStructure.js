e2e19d80cc991f22c11c05922cee20777aa240eb
herbatataszatana <anma.swiatkowska@gmail.com>
2021-07-19 10:34:27 +0100

    commit

---
+++ flexibleDataStructure.js
@@ -0,0 +1,20 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+  {
+  "artist": "The Strokes", 
+  "title" : "Is this it", 
+  "release_year" : 2002, 
+  "formats": 
+    ["vinyl", "CD"] 
+  }
+  ];
